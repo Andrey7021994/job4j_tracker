40490abf15c47ccdd30c051649045606ede2cb1a
andre <andreyklimenko@list.ru>
2024-01-22 00:04:08 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+public class DummyDic {
+
+    public String engToRus(String eng) {
+        String rus = "Привет";
+        return rus;
+    }
+
+    public static void main(String[] args) {
+        DummyDic jack = new DummyDic();
+        String eng = jack.engToRus("Hello");
+        System.out.println("Перевод:" + eng);
+    }
+}
