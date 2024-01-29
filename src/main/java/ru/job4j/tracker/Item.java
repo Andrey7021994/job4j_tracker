81509e8fa949b4c00c5ebcf313dae5e8853b8976
andre <andreyklimenko@list.ru>
2024-01-30 00:29:49 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,18 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
@@ -18,32 +30,5 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public Item() {
-    }
-
-    public Item(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Item(String name) {
-        this.name = name;
-    }
-
-    public void printInfo() {
-        System.out.println("Идентификатор: " + id);
-        System.out.println("Имя: " + name);
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-        Item first = new Item();
-        first.printInfo();
-        Item second = new Item(18, "Alex");
-        second.printInfo();
-        Item third = new Item("Alex");
-        third.printInfo();
     }
 }
