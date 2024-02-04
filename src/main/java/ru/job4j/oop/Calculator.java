1cb9da08ee8e93ac8809d22f4aa3c50d82a97f19
andre <andreyklimenko@list.ru>
2024-02-04 21:11:18 +0300

    Репозиторий под проект Tracker

--- src/main/java/ru/job4j/oop/Calculator.java
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -20,8 +20,8 @@ public class Calculator {
         return x + y;
     }
 
-    public int sumAllOperation(int y, int a1, int a2, int a3) {
-        int result = sum(y) + multiply(a1) + divide(a2) + minus(a3);
+    public int sumAllOperation(int y, int a) {
+        int result = sum(y) + multiply(a) + divide(a) + minus(a);
         return result;
     }
 
@@ -29,13 +29,13 @@ public class Calculator {
         int result1 = Calculator.sum(10);
         System.out.println(result1);
         Calculator calculator = new Calculator();
-        int result2 = calculator.multiply(5);
+        int result2 = calculator.multiply(6);
         System.out.println(result2);
-        int result3 = calculator.divide(15);
+        int result3 = calculator.divide(6);
         System.out.println(result3);
-        int result4 = calculator.minus(20);
+        int result4 = calculator.minus(6);
         System.out.println(result4);
-        int result5 = calculator.sumAllOperation(10, 5, 15, 20);
+        int result5 = calculator.sumAllOperation(10, 6);
         System.out.println(result5);
     }
 }
