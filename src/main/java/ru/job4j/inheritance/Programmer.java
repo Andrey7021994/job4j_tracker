fe2ee2be9e7599687ea13d7ccd98cf1bd86e1ac4
andre <andreyklimenko@list.ru>
2024-02-29 21:28:24 +0300

    3. Вызов конструктора родителя super(...) [#6837 #483268]

--- src/main/java/ru/job4j/inheritance/Programmer.java
+++ src/main/java/ru/job4j/inheritance/Programmer.java
@@ -1,6 +1,6 @@
 package ru.job4j.inheritance;
 
-public  class  Programmer extends Engineer {
+public class Programmer extends Engineer {
     private String programLanguage;
 
     public Programmer(String programLanguage, int experience, boolean degree) {
