72444f8c637004b7b8c78b7bf29de7046e873677
andre <andreyklimenko@list.ru>
2024-03-15 16:27:44 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,34 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book book1 = new Book("Triumphal Arch", 250);
+        Book book2 = new Book("Clean code", 380);
+        Book book3 = new Book("War and Peace", 1500);
+        Book book4 = new Book("Martin Eden", 480);
+        Book[] books = new Book[4];
+        books[0] = book1;
+        books[1] = book2;
+        books[2] = book3;
+        books[3] = book4;
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            System.out.println(book.getName() + " - " + book.getQuantity());
+        }
+
+        System.out.println("change Triumphal Arch and Martin Eden ");
+        books[0] = book4;
+        books[3] = book1;
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            System.out.println(book.getName() + " - " + book.getQuantity());
+        }
+        System.out.println("Shown only book.name Clean code");
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            if (book.getQuantity() == 380) {
+                System.out.println(book.getName() + " - " + book.getQuantity());
+            }
+        }
+    }
+}
