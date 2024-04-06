781e4af38f9a1eb39424e3fa6312fb6a45ab2d93
andre <andreyklimenko@list.ru>
2024-04-07 01:06:33 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,28 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] result = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
+            if (item != null) {
+                result[count++] = item;
+            }
+        }
+        return Arrays.copyOf(result, count);
+    }
+
+    Item[] findByName(String key) {
+        Item[] itemsByName = new Item[items.length];
+        int count = 0;
+        for (int index = 0; index < size; index++) {
+            if (items[index] != null && items[index].getName().equals(key)) {
+                itemsByName[count++] = items[index];
+            }
+        }
+        return Arrays.copyOf(itemsByName, count);
     }
 }
