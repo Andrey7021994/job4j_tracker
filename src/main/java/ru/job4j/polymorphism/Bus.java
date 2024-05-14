d5cd85b07b2fedd4ce7161dc1f97a5b6c5c95ac8
andre <andreyklimenko@list.ru>
2024-05-14 16:30:49 +0300

    3.6. Реализация класса StartUI. Вывод заявок по имени [#500749]

---
+++ src/main/java/ru/job4j/polymorphism/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.polymorphism;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int numberOfPassengers) {
+
+    }
+
+    @Override
+    public double fillUp(double amountOfFuel) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/polymorphism/Transport.java
@@ -0,0 +1,10 @@
+package ru.job4j.polymorphism;
+
+public interface Transport {
+    void drive();
+
+    void passengers(int numberOfPassengers);
+
+    double fillUp(double amountOfFuel);
+
+}
